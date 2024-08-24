a095d340f9f89fd0e38e86ad83b1c6ce523d280d
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-08-24 19:30:45 -0400

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -559,9 +559,10 @@ bool CheckTransaction(const CTransaction& tx, CValidationState &state, bool fChe
 
     if (tx.IsCoinBase())
     {
-        if (tx.vin[0].scriptSig.size() < 2 || tx.vin[0].scriptSig.size() > 150)
+        if (tx.vin[0].scriptSig.size() < 2 || tx.vin[0].scriptSig.size() > 150) {
             printf("Coinbase script size %d\n", tx.vin[0].scriptSig.size());
             return state.DoS(100, false, REJECT_INVALID, "bad-cb-length");
+        }
     }
     else
     {
