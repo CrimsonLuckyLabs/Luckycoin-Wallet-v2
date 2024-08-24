c3d8f51580c5e57ebf4593afe0566b6f75ab0001
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-08-24 04:19:18 -0400

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1582,7 +1582,7 @@ bool AbortNode(const std::string& strMessage, const std::string& userMessage="")
     SetMiscWarning(strMessage);
     LogPrintf("*** %s\n", strMessage);
     uiInterface.ThreadSafeMessageBox(
-        userMessage.empty() ? _(strMessage) : userMessage,
+        userMessage.empty() ? _(*strMessage) : userMessage,
         "", CClientUIInterface::MSG_ERROR);
     StartShutdown();
     return false;
