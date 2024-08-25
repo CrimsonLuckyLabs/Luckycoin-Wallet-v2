a240157738b725726abd33376b48846a05a1d7ce
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-08-25 04:54:13 -0400

    Update client.cpp

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -89,7 +89,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "getblock", 1, "verbose" },
     { "getblockheader", 1, "verbose" },
     { "gettransaction", 1, "include_watchonly" },
-    { "getrawtransaction", 1, "verbose" },
+    { "getrawtransaction", 2, "verbose" },
     { "createrawtransaction", 0, "inputs" },
     { "createrawtransaction", 1, "outputs" },
     { "createrawtransaction", 2, "locktime" },
