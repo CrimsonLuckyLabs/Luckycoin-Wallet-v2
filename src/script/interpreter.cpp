f1b44ef0761a90982d872e4301fc56f54c4d4bac
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-08-25 04:07:29 -0400

    Revert "Update interpreter.cpp"
    This reverts commit 1262419eaf2350ec41cdc0b1f9f66866a830584b.

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -176,10 +176,10 @@ bool static IsLowDERSignature(const valtype &vchSig, ScriptError* serror) {
     if (!IsValidSignatureEncoding(vchSig)) {
         return set_error(serror, SCRIPT_ERR_SIG_DER);
     }
-    std::vector<unsigned char> vchSigCopy(vchSig.begin(), vchSig.begin() + vchSig.size() - 1);
-    if (!CPubKey::CheckLowS(vchSigCopy)) {
-        return set_error(serror, SCRIPT_ERR_SIG_HIGH_S);
-    }
+    //std::vector<unsigned char> vchSigCopy(vchSig.begin(), vchSig.begin() + vchSig.size() - 1);
+    //if (!CPubKey::CheckLowS(vchSigCopy)) {
+    //    return set_error(serror, SCRIPT_ERR_SIG_HIGH_S);
+    //}
     return true;
 }
 
@@ -897,10 +897,10 @@ bool EvalScript(vector<vector<unsigned char> >& stack, const CScript& script, un
                         scriptCode.FindAndDelete(CScript(vchSig));
                     }
 
-                    if (!CheckSignatureEncoding(vchSig, flags, serror) || !CheckPubKeyEncoding(vchPubKey, flags, sigversion, serror)) {
+                    //if (!CheckSignatureEncoding(vchSig, flags, serror) || !CheckPubKeyEncoding(vchPubKey, flags, sigversion, serror)) {
                         //serror is set
-                        return false;
-                    }
+                    //    return false;
+                    //}
                     bool fSuccess = checker.CheckSig(vchSig, vchPubKey, scriptCode, sigversion);
 
                     if (!fSuccess && (flags & SCRIPT_VERIFY_NULLFAIL) && vchSig.size())
